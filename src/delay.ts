6fd5af8fea5026d54bdd899c6de26c886acf63aa
chenyh <1449101002@qq.com>
2022-07-21 10:35:23 +0800

    perf: delay

--- src/delay.ts
+++ src/delay.ts
@@ -1,9 +1,16 @@
 
+const CANCEL = 'CANCEL_PROMISE'
+
 /**
  * 延迟执行异步代码
  * @param delay 毫秒数
  * @returns Promise<void>
  */
 export default function delay<T = void>(delay: number, some?: T): Promise<T> {
-  return new Promise<T>(reslove => setTimeout(() => reslove(some), delay >= 0 ? delay : 0));
+  let timeoutId;
+  const promise = new Promise<T>(reslove => {
+    setTimeout(() => reslove(some as T), delay >= 0 ? delay : 0)
+  });
+  promise[CANCEL] = () => clearTimeout(timeoutId);
+  return promise;
 }
